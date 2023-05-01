99fb3816fe852d38ee509a485dfd9dd2c83ddd17
Gaurang Agrawal <99584477+Gaurang1602@users.noreply.github.com>
2023-05-01 18:35:00 +0530

    Update Majority_Element.java

--- Majority_Element.java
+++ Majority_Element.java
@@ -1,3 +1,43 @@
-
 Question-
   
+Majority Element
+MediumAccuracy: 27.82%Submissions: 451K+Points: 4
+Explore Job Fair for students & freshers for daily new opportunities. Find A Job Today! 
+
+Given an array A of N elements. Find the majority element in the array. A majority element in an array A of size N is an element that appears more than N/2 times in the array.
+ 
+
+Example 1:
+
+Input:
+N = 3 
+A[] = {1,2,3} 
+Output:
+-1
+Explanation:
+Since, each element in 
+{1,2,3} appears only once so there 
+is no majority element.
+Example 2:
+
+Input:
+N = 5 
+A[] = {3,1,3,3,2} 
+Output:
+3
+Explanation:
+Since, 3 is present more
+than N/2 times, so it is 
+the majority element.
+
+Your Task:
+The task is to complete the function majorityElement() which returns the majority element in the array. If no majority exists, return -1.
+ 
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(1).
+ 
+
+Constraints:
+1 ≤ N ≤ 107
+0 ≤ Ai ≤ 106
