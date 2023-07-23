eccfac8857c7e98669ffd39d6da968be65292768
Gaurang Agrawal <99584477+Gaurang1602@users.noreply.github.com>
2023-07-23 18:09:07 +0530

    Update NonRepeatingCharacter.java

--- NonRepeatingCharacter.java
+++ NonRepeatingCharacter.java
@@ -37,3 +37,26 @@ Solution-
 
 
 
+
+class Solution
+{
+    public:
+    //Function to find the first non-repeating character in a string.
+    char nonrepeatingCharacter(string S)
+    {
+        unordered_map<char,int>mp;
+        
+        for(auto x: S) 
+            mp[x]++;
+        
+        for(auto x: S)
+        {
+          if(mp[x]==1) 
+            return x;
+        }
+        
+        return '$';
+       
+    }
+
+};
