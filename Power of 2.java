41440126c0f480ec482d18018cd351881dff6d08
Gaurang Agrawal <99584477+Gaurang1602@users.noreply.github.com>
2023-09-18 01:28:24 +0530

    Update Power of 2.java

--- Power of 2.java
+++ Power of 2.java
@@ -1,2 +1,28 @@
 
 Question-
+
+
+Given a number N, check if N is power of 4 or not.
+
+Example 1:
+
+Input: 
+N = 64
+Output: 1
+Explanation:
+43= 64
+Example 2:
+
+Input: 
+N = 75
+Output : 0
+Explanation :
+75 is not a power of 4.
+Your task:
+You don't have to read input or print anything. Your task is to complete the function isPowerOfFour() which takes an integer N and returns 1 if the number is a power of four else returns 0.
+ 
+Expected Time Complexity: O(N)
+Expected Auxiliary Space : O(1)
+
+Constraints:
+1<=N<=105
