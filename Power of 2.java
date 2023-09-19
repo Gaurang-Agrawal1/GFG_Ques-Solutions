d20b08968bd52897901223eefa8382b9ddb644c5
Gaurang Agrawal <99584477+Gaurang1602@users.noreply.github.com>
2023-09-19 23:50:49 +0530

    Update Power of 2.java

--- Power of 2.java
+++ Power of 2.java
@@ -29,3 +29,21 @@ Constraints:
 
 
  Solution-
+
+
+ class Solution{
+    public:
+    // Function to check if given number n is a power of two.
+    bool isPowerofTwo(long long n){
+        
+        // Your code here
+         // Your code here    
+        if(n==0)
+        return 0;
+        
+        if((n&(n-1))==0)
+        return true;
+        return false;
+        
+    }
+};
