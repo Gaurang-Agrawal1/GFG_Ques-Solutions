8c681ecf779b98378cbdf7efbde76279a50c6abf
Gaurang Agrawal <99584477+Gaurang1602@users.noreply.github.com>
2024-02-16 17:44:34 +0530

    Update Flatten BST to sorted list.java

--- Flatten BST to sorted list.java
+++ Flatten BST to sorted list.java
@@ -65,3 +65,21 @@ Constraints:
 
 
 Solution-
+
+
+class Solution {
+     public Node flattenBST(Node root) {
+        Node temp = new Node(0);
+        moves(root, temp);
+        return temp.right;
+    }
+    private Node moves(Node node, Node temp) {
+        if (node == null)
+            return temp;
+        Node left = node.left;
+        Node right = node.right;
+        node.left = node.right = null;
+        moves(left, temp).right = node;
+        return moves(right, node);
+    }
+}
