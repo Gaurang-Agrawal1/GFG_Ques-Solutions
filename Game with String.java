150f0fb713ccba1aad817dd79e4a3daad427d6f6
Gaurang Agrawal <99584477+Gaurang1602@users.noreply.github.com>
2024-02-19 09:06:14 +0530

    Update Game with String.java

--- Game with String.java
+++ Game with String.java
@@ -35,3 +35,33 @@ Constraints:
 Solution-
 
 
+class Solution{
+    static int minValue(String s, int k){
+              int freq[] = new int[26];
+        for(int i = 0; i < s.length(); i++)
+            freq[s.charAt(i) - 'a']++;
+            
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder()); // max heap
+        for(int i = 0; i < 26; i++) {
+            if(freq[i] != 0)
+                pq.add(freq[i]);
+        }
+            
+        while(!pq.isEmpty() && k > 0) {
+            int t = pq.peek();
+            pq.poll();
+            pq.add(t - 1);
+            k--;
+        }
+        
+        int sum = 0;
+        while(!pq.isEmpty()) {
+            int t = pq.peek();
+            pq.poll();
+            sum += Math.pow(t, 2);
+        }
+        
+        return sum;
+
+    }
+}
