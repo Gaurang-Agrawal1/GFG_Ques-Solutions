866d32618ce7a3fcb9c67c4eb3cbd75917f2c525
Gaurang Agrawal <99584477+Gaurang1602@users.noreply.github.com>
2024-02-22 07:01:31 +0530

    Update Binary Tree Maximum Path Sum.java

--- Binary Tree Maximum Path Sum.java
+++ Binary Tree Maximum Path Sum.java
@@ -34,3 +34,25 @@ Solution-
 
 
 
+class Solution {
+    int max=Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        //most important
+        //path sum-->>( node value + Max(left,right));
+        //real life question
+        //konsa product kis bracnck me sabse zyada bika hai
+        //company kii kis branch me sabse zyada employee hai...
+        helper(root);
+        return max;
+    }
+    int helper(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        int left=Math.max(helper(root.left),0);
+        int right=Math.max(helper(root.right),0);
+
+        max=Math.max(max,root.val+left+right);
+        return root.val+Math.max(left,right);
+    }
+}
