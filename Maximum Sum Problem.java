a5c9955fe8b5a5757906cebd9e4470755a9d11d5
Gaurang Agrawal <99584477+Gaurang1602@users.noreply.github.com>
2024-02-24 19:36:26 +0530

    Update Maximum Sum Problem.java

--- Maximum Sum Problem.java
+++ Maximum Sum Problem.java
@@ -34,4 +34,16 @@ Constraints:
 Solution-
 
 
-
+class Solution
+{
+    public int maxSum(int n) 
+    { 
+        //code here.
+         int a, res=0;
+        if(n==0 || n==1){
+            return n;
+        }
+        a=Math.max(n,maxSum(n/2)+maxSum(n/3)+maxSum(n/4));
+        return a;
+    } 
+  }
