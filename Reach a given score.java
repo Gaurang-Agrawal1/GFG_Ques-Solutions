d6d89fb66877c24fa40e838277dc7f93cff0bc39
Gaurang Agrawal <99584477+Gaurang1602@users.noreply.github.com>
2024-02-25 17:51:20 +0530

    Update Reach a given score.java

--- Reach a given score.java
+++ Reach a given score.java
@@ -33,3 +33,20 @@ Constraints:
 Solution-
 
 
+
+class Geeks {
+    public long count(int amount) {
+        // Add your code here.
+         int[] coins={3,5,10};
+        long [] dp=new long[amount+1];
+        dp[0]=1;
+        
+        for(int i=0;i<coins.length;i++){
+            for(int j=coins[i];j<dp.length;j++){
+                dp[j]+=dp[j-coins[i]];
+            }
+        }
+        
+        return dp[amount];
+    }
+}
