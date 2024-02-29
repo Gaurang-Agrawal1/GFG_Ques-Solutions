513b6675c9f4221a19bbe00e8257ad8b4ff57ec8
Gaurang Agrawal <99584477+Gaurang1602@users.noreply.github.com>
2024-02-29 21:53:20 +0530

    Update Sum of bit differences.java

--- Sum of bit differences.java
+++ Sum of bit differences.java
@@ -40,3 +40,30 @@ Constraints:
 Solution-
 
 
+
+class Solution {
+    long sumBitDifferences(int[] arr, int n) {
+        // code here
+           long count = 0;
+
+        for (int i = 0; i < 32; i++) { 
+
+            int countSet = 0;
+
+            for (int num : arr) {
+
+                if ((num >> i & 1) == 1) {
+
+                    countSet++;
+
+                }
+
+            }
+
+            count += (countSet * (long) (n - countSet)) * 2; 
+
+        }
+
+        return count;
+    }
+}
