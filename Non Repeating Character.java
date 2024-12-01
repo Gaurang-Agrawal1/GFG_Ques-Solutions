326304abc7586ca1d24caf03cfc40623bec8d5ac
Gaurang Agrawal <99584477+Gaurang-Agrawal1@users.noreply.github.com>
2024-12-01 17:39:37 +0530

    Update Non Repeating Character.java

--- Non Repeating Character.java
+++ Non Repeating Character.java
@@ -19,3 +19,23 @@ Explanation: All the characters in the given string are repeating.
 Constraints:
 1 <= s.size() <= 105
 
+Solution-
+
+
+class Solution {
+    // Function to find the first non-repeating character in a string.
+    static char nonRepeatingChar(String s) {
+        // Your code here
+        HashMap<Character, Integer> map=new HashMap<>();
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+        }
+        for(int i=0;i<n;i++){
+            if(map.get(s.charAt(i))==1){
+                return s.charAt(i);
+            }
+        }
+        return '$';
+    }
+}
