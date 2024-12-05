dd24583d1842cb16a0935fa2470d78bb2095e6f2
Gaurang Agrawal <99584477+Gaurang-Agrawal1@users.noreply.github.com>
2024-12-05 21:08:17 +0530

    Update Sort 0s, 1s and 2s.java

--- Sort 0s, 1s and 2s.java
+++ Sort 0s, 1s and 2s.java
@@ -15,3 +15,33 @@ Constraints:
 1 <= arr.size() <= 106
 0 <= arr[i] <= 2
 
+Solution-
+
+void sort012(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        int _0s = 0;
+        int _2s = n-1;
+        int i = 0;
+        
+        while( i <= _2s)
+        {
+            if(arr[i] == 0)
+            {
+                arr[i] = arr[_0s];
+                arr[_0s] = 0;
+                _0s++;
+                i++;
+            }
+            else if(arr[i] == 2)
+            {
+                arr[i] = arr[_2s];
+                arr[_2s] = 2;
+                _2s--;
+            }
+            else
+            {
+                i++;
+            }
+        }
+    }
