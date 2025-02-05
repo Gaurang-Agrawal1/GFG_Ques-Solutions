cac2d0f50464a3cc9d654feba989f647d182d8bc
Gaurang Agrawal <99584477+Gaurang-Agrawal1@users.noreply.github.com>
2025-02-05 18:15:34 +0530

    Update Mirror Tree.java

--- Mirror Tree.java
+++ Mirror Tree.java
@@ -20,3 +20,33 @@ Constraints:
 1 ≤ number of nodes ≤ 105
 1 ≤ node->data ≤ 105
 
+
+Solution-
+
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+   Node(int item)
+   {
+        data = item;
+        left = right = null;
+    }
+} */
+
+class Solution {
+    // Function to convert a binary tree into its mirror tree.
+    void mirror(Node node) {
+        // Your code here
+         if( node == null){
+            return;
+         }
+          Node temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+        mirror(node.right);
+        mirror(node.left);
+    }
+    
+}
