cc3d10da235e632037a1b2bed4c5ddfb3594d17b
Gaurang Agrawal <99584477+Gaurang-Agrawal1@users.noreply.github.com>
2025-03-08 22:39:31 +0530

    Update Longest Palindrome in a String.java

--- Longest Palindrome in a String.java
+++ Longest Palindrome in a String.java
@@ -27,3 +27,29 @@ s consist of only lowercase English letters.
 Solution-
 
 
+
+class Solution {
+    static String expandAround(String s,int i,int j){
+        while(i>=0 && j<s.length() && s.charAt(i)==s.charAt(j)){
+            i--;
+            j++;
+        }
+        return s.substring(i+1,j);
+    }
+    static String longestPalindrome(String s) {
+        if(s.length()==0||s==null)return "";
+        String longestPalin="";
+        for(int i=0;i<s.length();i++){
+            String pal1=expandAround(s,i,i+1);
+            if(pal1.length()>longestPalin.length()){
+                longestPalin=pal1;
+            }
+            String pal2=expandAround(s,i,i);
+            if(pal2.length()>longestPalin.length()){
+                longestPalin=pal2;
+            }
+        }
+        return longestPalin;
+    }
+}
+
