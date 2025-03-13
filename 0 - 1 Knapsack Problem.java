8d1a5be1df518fe9d215f8c4c4f29871444724e3
Gaurang Agrawal <99584477+Gaurang-Agrawal1@users.noreply.github.com>
2025-03-13 22:54:26 +0530

    Update 0 - 1 Knapsack Problem.java

--- 0 - 1 Knapsack Problem.java
+++ 0 - 1 Knapsack Problem.java
@@ -20,3 +20,24 @@ Constraints:
 1 ≤ W ≤ 103
 1 ≤ val[i] ≤ 103
 1 ≤ wt[i] ≤ 103
+
+
+
+Solution-
+
+
+class Solution {
+    static int knapsack(int W, int val[], int wt[]) {
+        // code here
+            int n=val.length;
+        int dp[]=new int[W+1];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=W;j>=wt[i];j--)
+            {
+                dp[j]=Math.max(dp[j],val[i]+dp[j-wt[i]]);
+            }
+        }
+        return dp[W];
+    }
+}
